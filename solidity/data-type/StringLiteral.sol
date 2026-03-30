c9670ebdcc673c068eeecfdc53494a2967c89011
LeeChanhyeok <ranya230@gmail.com>
2026-03-30 20:32:23 +0900

    week4: alchemy

---
+++ solidity/data-type/StringLiteral.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract StringLiteral {
+	bytes32 public msg1 = "Hello World";
+    string public msg2 = "over the 32bytes string testtesttest";
+}
+
---
+++ solidity/function/ViewAdd.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ViewAdd {
+    uint public x;
+
+    constructor(uint a){
+        x = a;
+    }
+    
+    function increment() external {
+        x += 1;
+    }
+
+    function add(uint _a) external view returns(uint){
+        return x+_a;
+    }
+}
